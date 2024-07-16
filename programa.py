0bc381b26c44f87866b8a3d20094d1a5704c2e30
estrellah <coozystar@gmail.com>
2024-07-16 12:36:09 +0200

    borrado cosas

--- programa.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-# Configura la conexión serial
-ser = serial.Serial('/dev/ttyUSB0', 115200)  # Ajusta el puerto serial según corresponda (por ejemplo, en Windows podría ser 'COM3')
-
-while True:
-    if ser.in_waiting > 0:
-        mensaje = ser.readline().decode('utf-8').rstrip()  # Lee la línea y decodifica desde bytes a string UTF-8
-        print(f'Mensaje recibido desde Arduino: {mensaje}')
